4368ae60fb566d90cc466f98619d87b3db512b18
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2021-12-07 12:54:23 +0000

    Update main.rs

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,5 @@
 mod load_data;
-use std::{io::BufRead, fs};
+use std::{ fs, clone };
 
 use load_data::*;
 
@@ -16,27 +16,56 @@ pub struct Data {
 }
 
 fn main() {
-    part1();
-    //part2();
+    //part1();
+    part2();
 }
 
 
 fn part1() {
-    let data: Vec<u32> = fs::read_to_string("input")
+    let data: Vec<i32> = fs::read_to_string("input")
         .expect("File not found")
         .trim()
         .split(",")
         .map(|f| f.parse().unwrap())
         .collect();
     
-    
-    println!("{:?}", av)
+    let max = &data.iter().max().unwrap().clone();
+    let min = &data.iter().min().unwrap().clone();
+
+    let mut lowest_cost = i32::MAX;
+    for i in min.clone()..max.clone() {
+        let mut cost = 0;
+        for point in &data{
+            cost += ( point - i).abs();
+        }
+        if cost < lowest_cost{
+            lowest_cost = cost;
+        } 
+    }
+    println!("{:?}", lowest_cost)
 }
 
 fn part2() {
-    let data = read_lines_into_data("input");
+    let data: Vec<i64> = fs::read_to_string("input")
+        .expect("File not found")
+        .trim()
+        .split(",")
+        .map(|f| f.parse().unwrap())
+        .collect();
+    
+    let max = &data.iter().max().unwrap().clone();
+    let min = &data.iter().min().unwrap().clone();
 
-    for line in data {
-        
+    let mut lowest_cost = i64::MAX;
+    for i in min.clone()..(max.clone() + 1) {
+        let mut cost = 0;
+        for point in &data{
+            let n = ( point - i).abs();
+            cost += ( n * (n + 1) ) / 2;
+        }
+        if cost < lowest_cost{
+            lowest_cost = cost;
+        } 
     }
+    println!("{:?}", lowest_cost)
 }
