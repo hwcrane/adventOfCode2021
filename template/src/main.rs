7dce6090d126f759c8bbb693e46bccb76cbb502f
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2021-12-14 00:56:33 +0000

    modified template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,19 +1,5 @@
-mod load_data;
 use std::time::Instant;
-
-use load_data::*;
-
-extern crate recap;
-use recap::Recap;
-use serde::Deserialize;
-
-
-#[derive(Debug, Recap, Deserialize)]
-#[recap(regex=r#"(?P<item1>\S+)\s(?P<item2>\S+)"#)]
-pub struct Data {
-    item1: String,
-    item2: String
-}
+use std::fs;
 
 fn main() {
     let start = Instant::now();
@@ -25,17 +11,9 @@ fn main() {
 
 
 fn part1() {
-    let data = read_lines_into_data("input");
 
-    for line in data {
-        
-    }
 }
 
 fn part2() {
-    let data = read_lines_into_data("input");
 
-    for line in data {
-        
-    }
 }
